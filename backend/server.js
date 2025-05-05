ed933407472479a5fb0ab2c71b5b2bc3af3197e2
HDC <118044474+Himaja-HD@users.noreply.github.com>
2025-05-05 12:50:17 +0530

    Auth System (JWT)

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+import protect from './middleware/authMiddleware.js';
 
 dotenv.config();
 const app = express();
@@ -11,10 +13,13 @@ app.use(express.json());
 // Connect DB
 connectDB();
 
-// Base route
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+// route
+app.use('/api/auth', authRoutes);
+
+// protected route
+app.get('/api/protected', protect, (req, res) => {
+    res.json({ message: `Welcome ${req.user.username}` });
+  });
 
 // Start server
 const PORT = process.env.PORT || 5000;
