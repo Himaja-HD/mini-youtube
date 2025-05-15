49afb0ad802139230ebfa4d1228efeef131cd263
HDC <118044474+Himaja-HD@users.noreply.github.com>
2025-05-15 14:33:17 +0530

    Delete backend/routes/searchRoutes.js

--- backend/routes/searchRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { searchVideos } from "../controllers/searchController.js"
-
-const router = express.Router()
-
-router.get("/", searchVideos)
-
-export default router
