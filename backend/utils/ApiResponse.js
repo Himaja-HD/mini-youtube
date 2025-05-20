4b2deefc73f913ceb382ce99aed57a2cb2319d63
HDC <118044474+Himaja-HD@users.noreply.github.com>
2025-05-20 12:03:31 +0530

    Delete backend directory

--- backend/utils/ApiResponse.js
+++
@@ -1,9 +0,0 @@
-export default class ApiResponse {
-    constructor(statusCode, data, message = "Success") {
-      this.statusCode = statusCode;
-      this.data = data;
-      this.message = message;
-      this.success = statusCode >= 200 && statusCode < 300;
-    }
-  }
-  
--- backend/utils/generateToken.js
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const generateToken = (userId, username, role) => {
-  return jwt.sign({ userId, username, role }, process.env.JWT_SECRET, { expiresIn: '30d' });
-};
-
-export default generateToken;
